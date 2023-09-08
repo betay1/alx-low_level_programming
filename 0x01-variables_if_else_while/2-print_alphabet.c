551f8ac5b3580e15ebda6fce035a26d9d1fdf89f
betay1 <betelhemaynalem0@gmail.com>
2023-09-08 14:53:49 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Return: 0
+ */
+
+int main(void)
+
+{
+char n;
+
+for (n = 'a'; n <= 'z'; n++);
+putchar(n);
+putchar('\n');
+
+return (0);
+
+}
+
