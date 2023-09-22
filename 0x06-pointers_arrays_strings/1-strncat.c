3888567a6233479889f130044d953fc96213ded2
betay1 <betelhemaynalem0@gmail.com>
2023-09-21 18:56:47 -0700

    1,2,3-strcmp.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
  * _strncat - concatenate two strings
- * using at most n bytes from src	
+ *
  * @dest: input value
  * @src: input value
  * @n: input value
  *
- * Return: dest	
+ * Return: dest
  */
 char *_strncat(char *dest, char *src, int n)
 {
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-		i++;
+	i++;
 	}
 	j = 0;
 	while (j < n && src[j] != '\0')
@@ -28,4 +28,3 @@ char *_strncat(char *dest, char *src, int n)
 	dest[i] = '\0';
 	return (dest);
 }
-
--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,17 +12,16 @@ char *_strncpy(char *dest, char *src, int n)
 	int j;
 
 	j = 0;
-	while (j < n && src[j] != '\0')	
+	while (j < n && src[j] != '\0')
 	{
-		dest[j] = src[j];
-		j++;
+	dest[j] = src[j];
+	j++;
 	}
 	while (j < n)
 	{
-		dest[j] = '\0';
-		j++;
+	dest[j] = '\0';
+	j++;
 	}
 
 	return (dest);
 }
-
