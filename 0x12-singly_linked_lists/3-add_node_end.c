e5fa156a3542fd654a38ffd21aa7d40b7a78fd34
betay1 <betelhemaynalem0@gmail.com>
2023-10-19 14:52:25 -0700

    1,2,3,4-free_lists

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,17 +12,17 @@ list_t *add_node_end(list_t **head, const char *str)
 {
 list_t *new;
 list_t *temp = *head;
-unsigned int nod1 = 0;
+unsigned int len = 0;
 
-while (str[nod1])
-nod1++;
+while (str[len])
+len++;
 
 new = malloc(sizeof(list_t));
 if (!new)
 return (NULL);
 
 new->str = strdup(str);
-new->nod1 = nod1;
+new->len = len;
 new->next = NULL;
 
 	if (*head == NULL)
