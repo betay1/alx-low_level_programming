19b84caf9d94b11a7131c9da34d88e189df9db79
betay1 <betelhemaynalem0@gmail.com>
2023-11-03 07:44:46 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 	int nletters;
 	int rwr;
 
-	if (filename == Null)
+	if (filename == NULL)
 		return (0);
 
 	file_1 = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
