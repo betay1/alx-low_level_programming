9f796389e2bb17e3565e9427e5689c7f5b7c8b22
betay1 <betelhemaynalem0@gmail.com>
2023-11-03 08:16:40 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -32,42 +32,42 @@ void error_file(int file_from, int file_to, char *argv[])
 
 int main(int argc, char *argv[])
 {
-	int file_1, file_2, err_close;
+	int file_from, file_to, err_close;
 	ssize_t nchar, nwr;
 	char buff[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_1 file_2");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 
-	file_1 = open(argv[1], O_RDONLY);
-	file_2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_1, file_2, argv);
+	file_from = open(argv[1], O_RDONLY);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_file(file_from, file_to, argv);
 
 	nchar = 1024;
 	while (nchar == 1024)
 	{
-		nchar = read(file_1, buff, 1024);
-		if (nchas == -1)
+		nchar = read(file_from, buff, 1024);
+		if (nchar == -1)
 			error_file(-1, 0, argv);
-		nwr = write(file_2, buff, nchar);
+		nwr = write(file_to, buff, nchar);
 		if (nwr == -1)
 			error_file(0, -1, argv);
 	}
 
-	err_close = close(file_1);
+	err_close = close(file_from);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 
-	err_close = close(file_2);
+	err_close = close(file_to);
 	if (err_close == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_1);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
 		exit(100);
 	}
 	return (0);
